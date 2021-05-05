cfeae5174220d946d4115fe6aaf3e2f983bae299
rune samyn <rune.samyn@gmail.com>
2021-05-05 22:38:05 +0200

    change color toggle switch

--- public/scripts/settings.js
+++ public/scripts/settings.js
@@ -2,7 +2,11 @@ document.getElementById('input-push-notifications-enable').addEventListener('cli
     var span = document.querySelector('#input-push-notifications-enable > .material-icons');
     if(span.innerText == "toggle_on") {
         span.innerText = "toggle_off";
+        span.classList.add('text-red-600')
+        span.classList.remove('text-green-600')
     } else if(span.innerText == "toggle_off") {
         span.innerText = "toggle_on";
+        span.classList.remove('text-red-600')
+        span.classList.add('text-green-600')
     }
 })
