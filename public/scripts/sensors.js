a485ba4d7a94f539b8f0e7afae0dded89f92205f
rune samyn <rune.samyn@gmail.com>
2021-05-11 00:44:32 +0200

    qr code scanner and notifcations enable

--- public/scripts/sensors.js
+++ public/scripts/sensors.js
@@ -2,29 +2,100 @@ var socket = io();
 
 var firstData = true;
 var rooms = [];
+var roomsSubscribedTo = [];
+var _mediaStream = null;
+var video = document.createElement('video');
+var handleAnimationFrame;
+var subscriptionId = ""
 
 window.addEventListener('load', () => {
-    axios.get('/getRooms')
-        .then(response => {
-            rooms = response.data;
-            makeCards(rooms);
+    // get rooms where user is subscribed to.
+    if ('subscriptionId' in localStorage) {
+        // check if you have already a pushID
+        subscriptionId = localStorage.getItem('subscriptionId')
+        if (subscriptionId != "") {
+            // check if ID needs to be updated
 
-            var liveDataInterval = setInterval(() => {
-                socket.emit('get live data');
-            }, 1000)
+            axios.post('/api/check-push-id/', { ID: subscriptionId })
+                .then(async (response) => {
+                    if (response.data.data.success) {
+                        console.log("subscription already exists")
+                    } else {
+                        // make new subscription
+                        console.log("make new subscription")
+                        await subscribeToNotifications();
+                    }
+                    axios.post('/api/get-rooms-by-id/', { ID: subscriptionId })
+                        .then(response => {
+                            console.log("response: " + JSON.stringify(response))
+                            if (response.data.data.success) {
+                                roomsSubscribedTo = response.data.data.rooms;
+                                console.log("roomsSubscribedTo: " + roomsSubscribedTo);
+                            }
+                            axios.get('/getRooms')
+                                .then(response => {
+                                    rooms = response.data;
+                                    makeCards(rooms);
 
-            socket.on('live data', data => {
-                updateCards(data);
-            })
+                                    var liveDataInterval = setInterval(() => {
+                                        socket.emit('get live data');
+                                    }, 1000)
 
-            socket.on('connect_error', function () {
-                noConnection();
-            })
-        })
-        .catch(err => {
-            console.log(err);
-        })
+                                    socket.on('live data', data => {
+                                        updateCards(data);
+                                    })
+
+                                    socket.on('connect_error', function () {
+                                        noConnection();
+                                    })
+                                })
+                                .catch(err => {
+                                    console.log(err);
+                                })
+                        })
+                        .catch(err => {
+                            console.log(err)
+                        })
+                })
+                .catch(err => {
+                    console.log("failed to check subscription: " + err)
+                })
+        } else {
+            axios.get('/getRooms')
+                .then(response => {
+                    rooms = response.data;
+                    makeCards(rooms);
+
+                    var liveDataInterval = setInterval(() => {
+                        socket.emit('get live data');
+                    }, 1000)
+
+                    socket.on('live data', data => {
+                        updateCards(data);
+                    })
+
+                    socket.on('connect_error', function () {
+                        noConnection();
+                    })
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }
+    }
+    // Zoek naar media devices.
+    if ('mediaDevices' in navigator) {
+
+        // Enumereren over de devices en beschikbaar maken in select.
+        // Zie: https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/enumerateDevices.
+
+
+    }
+    else {
+        alert('No media devices support in this browser.');
+    }
 })
+
 
 // functions
 
@@ -41,12 +112,38 @@ function searchSensors() {
     })
 }
 
-function toggleNotification(room) {
+async function toggleNotification(room) {
     let span = document.querySelector(`[data-span-notification-room="${room}"]`);
     if (span.innerText == "notifications_active") {
         span.innerText = "notifications_off";
+        // remove room from list of subscriptions
+        roomsSubscribedTo.splice(roomsSubscribedTo.indexOf(room), 1)
+        axios.post('/api/remove-id-from-room/', { room: room, id: subscriptionId })
+            .then(response => {
+                if (response.data.data.success) {
+                    console.log("successfully removed subscription to room")
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            })
     } else if (span.innerText == "notifications_off") {
-        span.innerText = "notifications_active";
+        // add room to list of subscriptions
+        if (subscriptionId == "") {
+            alert('You need to enable the notifications in the settings page!')
+        } else {
+            span.innerText = "notifications_active";
+            roomsSubscribedTo.push(room)
+            axios.post('/api/add-id-to-room/', { room: room, id: subscriptionId })
+                .then(response => {
+                    if (response.data.data.success) {
+                        console.log("successfully added subscription to room")
+                    }
+                })
+                .catch(err => {
+                    console.log(err);
+                })
+        }
     }
 }
 
@@ -57,7 +154,6 @@ function makeCards(rooms) {
         card.setAttribute("data-card", room);
         card.classList.remove("hidden");
 
-        console.log(card)
         // change room name
         card.querySelector('span[data-room="template"]').innerText = "Lokaal " + room;
         card.querySelector('span[data-room="template"]').setAttribute("data-room", room);
@@ -66,7 +162,10 @@ function makeCards(rooms) {
         card.querySelector('[data-button-notification="template"]').addEventListener("click", () => toggleNotification(room));
         card.querySelector('[data-button-notification="template"]').setAttribute('data-button-notification', room);
 
-        // change notifications span data- atribute
+        // check if notifications are enabled
+        if (roomsSubscribedTo.includes(room)) {
+            card.querySelector('[data-span-notification-room="template"]').innerText = "notifications_active";
+        }
         card.querySelector('[data-span-notification-room="template"]').setAttribute('data-span-notification-room', room);
 
         // change graph url
@@ -134,6 +233,80 @@ function noConnection() {
             card.querySelector(`span[data-error="${room}"]`).classList.remove('hidden');
         }
     })
+}
+
+function openCamera() {
+    var cameraWindow = document.getElementById('camera-window');
+    var sensorWindow = document.getElementById('sensor-window');
+    var btn = document.getElementById('btn-goToURL');
+    cameraWindow.classList.remove('hidden');
+    sensorWindow.classList.add('overflow-hidden');
+    btn.innerText = `no QR-code`;
+    btn.addEventListener('click', () => { goToURL('#') })
+    // Use facingMode: environment to attemt to get the front camera on phones
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then(function (stream) {
+        _mediaStream = stream;
+        video.srcObject = _mediaStream;
+        video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+        video.play();
+    });
+    handleAnimationFrame = window.requestAnimationFrame(tick);
+}
+
+function closeCamera() {
+    var cameraWindow = document.getElementById('camera-window');
+    var sensorWindow = document.getElementById('sensor-window');
+    cameraWindow.classList.add('hidden');
+    sensorWindow.classList.remove('overflow-hidden');
+    window.cancelAnimationFrame(handleAnimationFrame);
+
+    // Eventuele oude streams afsluiten (camera vrijgeven).
+    if (_mediaStream != null) {
+        _mediaStream.getTracks().forEach(track => {
+            track.stop();
+        });
+    }
+}
+
+function drawLine(begin, end, color) {
+    var canvas = document.getElementById('qr-canvas').getContext('2d');
+    canvas.beginPath();
+    canvas.moveTo(begin.x, begin.y);
+    canvas.lineTo(end.x, end.y);
+    canvas.lineWidth = 4;
+    canvas.strokeStyle = color;
+    canvas.stroke();
+}
+
+function tick() {
+    var qrCanvasElement = document.getElementById('qr-canvas');
+    var qrCanvas = document.getElementById('qr-canvas').getContext('2d');
+    if (video.readyState === video.HAVE_ENOUGH_DATA) {
+        qrCanvasElement.classList.remove('hidden')
+
+        qrCanvasElement.height = video.videoHeight;
+        qrCanvasElement.width = video.videoWidth;
+        qrCanvas.drawImage(video, 0, 0, qrCanvasElement.width, qrCanvasElement.height);
+        var imageData = qrCanvas.getImageData(0, 0, qrCanvasElement.width, qrCanvasElement.height);
+        var code = jsQR(imageData.data, imageData.width, imageData.height, {
+            inversionAttempts: "dontInvert",
+        });
+        if (code && code.data.includes('http')) {
+            drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
+            drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
+            drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
+            drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
+            var btn = document.getElementById('btn-goToURL');
+            btn.innerText = `Go to '${code.data}'`;
+            btn.addEventListener('click', () => goToURL(code.data));
+        }
+    }
+    handleAnimationFrame = window.requestAnimationFrame(tick);
+}
+
+function goToURL(url) {
+    console.log(`go to ${url}`)
+    window.location.href = url;
 }
 
 
